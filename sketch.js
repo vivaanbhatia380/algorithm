beb4cc4dfd8efc27166809a2dd4bd3e89e657447
vivaanbhatia380 <71877612+vivaanbhatia380@users.noreply.github.com>
2020-10-05 15:58:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var fixedrect ,movingrect;
+function setup() {
+  createCanvas(800,400);
+ fixedrect= createSprite(200, 200, 50, 50);
+ movingrect= createSprite(200, 200, 50, 50);
+ fixedrect.shapeColor="green";
+ movingrect.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255);  
+  movingrect.x=mouseX;
+  movingrect.y=mouseY;
+  if(movingrect.x-fixedrect.x<movingrect.width/2+fixedrect.width/2&&
+    fixedrect.x-movingrect.x<movingrect.width/2+fixedrect.width/2&&
+    movingrect.y-fixedrect.y<movingrect.height/2+fixedrect.height/2&&
+    fixedrect.y-movingrect.y<movingrect.height/2+fixedrect.height/2){
+      fixedrect.shapeColor="red";
+      movingrect.shapeColor="red";
+    }
+    else{
+      fixedrect.shapeColor="green";
+ movingrect.shapeColor="green";
+    }
+  drawSprites();
+}
